4260ce869dc4ed0910e6b8102fa0a61858ed45bb
koudaiii <cs006061@gmail.com>
2017-01-23 10:41:58 +0900

    Update sha256

--- Formula/qcli.rb
+++ Formula/qcli.rb
@@ -4,7 +4,7 @@ class Qcli < Formula
   desc ""
   homepage "https://github.com/koudaiii/qcli"
   url "https://github.com/koudaiii/qcli/releases/download/#{VERSION}/qcli-#{VERSION}-darwin-amd64.tar.gz"
-  sha256 "e68d1eb73bdb2db281bd70126840453030aaea88669dee62e8b27be7323d7cd6"
+  sha256 "45c68a6ebbaa91bcc23406aca70267a5f21722433bc324702ece675d3a6428b2"
 
   def install
     bin.install "qcli"
