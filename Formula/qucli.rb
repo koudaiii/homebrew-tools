b8f14bd7ba7b3f20a597b5801525b834256bbba6
koudaiii <cs006061@gmail.com>
2017-01-24 10:46:35 +0900

    Bump up to v0.4.0

--- Formula/qucli.rb
+++ Formula/qucli.rb
@@ -1,10 +1,10 @@
 class Qucli < Formula
-  VERSION = "v0.3.0"
+  VERSION = "v0.4.0"
 
   desc ""
   homepage "https://github.com/koudaiii/qucli"
   url "https://github.com/koudaiii/qucli/releases/download/#{VERSION}/qucli-#{VERSION}-darwin-amd64.tar.gz"
-  sha256 "45c68a6ebbaa91bcc23406aca70267a5f21722433bc324702ece675d3a6428b2"
+  sha256 "c60265dcbfe06a1438c27f3fe7884c7a681f86a2fc53c025f70c73cf3e58a00e"
 
   def install
     bin.install "qucli"
