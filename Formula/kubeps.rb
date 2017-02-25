a64dfbf19dd3d2906538587e6462e61f6c93cd55
koudaiii <cs006061@gmail.com>
2017-02-26 03:35:26 +0900

    Fix typo

--- Formula/kubeps.rb
+++ Formula/kubeps.rb
@@ -4,7 +4,7 @@ class Kubeps < Formula
   desc "Get container image tag for Kubernetes Pods"
   homepage "https://github.com/koudaiii/kubeps"
   url "https://github.com/koudaiii/kubeps/releases/download/v#{VERSION}/kubeps-v#{VERSION}-darwin-amd64.tar.gz"
-  version VESION
+  version VERSION
   sha256 "28161623ac35a9a1417053208bc897098963143b6e97d7edcf2fe743f5615ab4"
 
   def install
