ce8a0c3c407ac4a5faf0a27b2faf236399c717ff
koudaiii <cs006061@gmail.com>
2017-02-25 11:02:14 +0900

    Fix versoning

--- Formula/kubeps.rb
+++ Formula/kubeps.rb
@@ -1,9 +1,10 @@
 class Kubeps < Formula
-  VERSION = "v0.2.1"
+  VERSION = "0.2.1".freeze
 
   desc ""
   homepage "https://github.com/koudaiii/kubeps"
-  url "https://github.com/koudaiii/kubeps/releases/download/#{VERSION}/kubeps-#{VERSION}-darwin-amd64.tar.gz"
+  url "https://github.com/koudaiii/kubeps/releases/download/v#{VERSION}/kubeps-v#{VERSION}-darwin-amd64.tar.gz"
+  version VESION
   sha256 "28161623ac35a9a1417053208bc897098963143b6e97d7edcf2fe743f5615ab4"
 
   def install
