d12f7d4607a06d8909a6f83f669520452854a44b
koudaiii <cs006061@gmail.com>
2017-09-15 12:52:16 +0900

    Update kubeps

--- Formula/kubeps.rb
+++ Formula/kubeps.rb
@@ -1,11 +1,11 @@
 class Kubeps < Formula
-  VERSION = "0.2.1".freeze
+  VERSION = "0.2.3".freeze
 
   desc "Get container image tag for Kubernetes Pods"
   homepage "https://github.com/koudaiii/kubeps"
   url "https://github.com/koudaiii/kubeps/releases/download/v#{VERSION}/kubeps-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "28161623ac35a9a1417053208bc897098963143b6e97d7edcf2fe743f5615ab4"
+  sha256 "ab5f14b23850c3ffc63a28541ddbb14fd4665afce3b183cf4b94d225b559509b"
 
   def install
     bin.install "kubeps"
