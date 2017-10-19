2d26265d038dd421a94b56ade1ceae1b1065b6d0
koudaiii <cs006061@gmail.com>
2017-10-19 11:44:06 +0900

    Bump up to 0.6.5 qucli

--- Formula/qucli.rb
+++ Formula/qucli.rb
@@ -1,11 +1,11 @@
 class Qucli < Formula
-  VERSION = "0.6.4".freeze
+  VERSION = "0.6.5".freeze
 
   desc "Manage repositories in Quay.io"
   homepage "https://github.com/koudaiii/qucli"
   url "https://github.com/koudaiii/qucli/releases/download/v#{VERSION}/qucli-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "9134312f5897822750cf35e3cf3f80f45fdc00a9eeaef0c46bc90c4bd34be2c1"
+  sha256 "e00a307617c58ff415ef36e542cd1a58c68d3833332dbc7dfe8febe00b9fce42"
 
   def install
     bin.install "qucli"
